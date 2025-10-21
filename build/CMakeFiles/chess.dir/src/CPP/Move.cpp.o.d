32abb4ebac6cdb4b5777794b6135ec69b3e03108
Janis Rolf <janis.rolf@stud.uni-goettingen.de>
2025-10-21 12:45:42 +0200

    build ordner entfert

--- build/CMakeFiles/chess.dir/src/CPP/Move.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/chess.dir/src/CPP/Move.cpp.o: \
- /home/janis-rolf/code/cpp/chess/src/CPP/Move.cpp \
- /usr/include/stdc-predef.h \
- /home/janis-rolf/code/cpp/chess/src/Header/Move.h \
- /home/janis-rolf/code/cpp/chess/src/Header/Color.h \
- /home/janis-rolf/code/cpp/chess/src/Header/Constants.h
